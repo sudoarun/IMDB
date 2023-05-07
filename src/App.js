5616d1b5c067acd645ea1e105b6fb77cc5b371b3
sudoarun <arurajorya1999@gmail.com>
2023-05-07 20:20:01 +0530

    release 3

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import 'mdb-ui-kit/css/mdb.min.css';
+import 'mdb-ui-kit/js/mdb.min.js';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import SignIn from './components/SignIn';
+import WatchList from './components/WatchList';
+import Movie from './components/Movie';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/watchlist' element={<WatchList />} />
+        <Route path='/movie/:title' element={<Movie />} />
+      </Routes>
+    </div>
+
+  );
+}
+
+export default App;
